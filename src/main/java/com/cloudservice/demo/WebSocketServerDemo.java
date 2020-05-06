0a9da97a5b8125d5d3555d3a7363761c2328a50a
17061934 <17061934"suning.com>
2020-05-06 21:37:05 +0800

    base

---
+++ src/main/java/com/cloudservice/demo/WebSocketServerDemo.java
@@ -0,0 +1,27 @@
+package com.cloudservice.demo;
+/*
+ * Copyright (C), 2002-2018, 苏宁易购电子商务有限公司
+ * FileName: com.cloudservice.demo
+ * Author:   陈晨(96421)
+ * Date:     2020/5/6 21:34
+ * Description: // 模块目的、功能描述
+ * History:		// 修改记录
+ * <author>		<time>		<version>		<desc>
+ * 96421		2020/5/6 21:34		1.0		<描述>
+ */
+
+/**
+ * @version v1.0
+ * @Description: TODO
+ * <p>〈功能详细描述〉</p>
+ *
+ * <pre>
+ * 〈举例说明〉
+ * </pre>
+ * @ClassName WebSocketServerDemo
+ * @author 陈晨(96421955@163.com)
+ * @date 2020/5/6 21:34
+ */
+public class WebSocketServerDemo {
+
+}
