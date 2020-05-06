635942e15b9753d47c1ddd2226f6fcc2b5ffbfcc
17061934 <17061934"suning.com>
2020-05-06 21:30:55 +0800

    base

--- src/main/java/com/cloudservice/action/CloudServiceApplication.java
+++ src/main/java/com/cloudservice/action/CloudServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ActionApplication {
+public class CloudServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ActionApplication.class, args);
+        SpringApplication.run(CloudServiceApplication.class, args);
     }
 
 }
