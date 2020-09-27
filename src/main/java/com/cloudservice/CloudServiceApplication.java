6a4028dbe0a56d9d3fce688c2819e1096d8f521e
chenchen <chenchen@keepbtc.com>
2020-09-27 15:38:28 +0800

    basis

--- src/main/java/com/cloudservice/CloudServiceApplication.java
+++ src/main/java/com/cloudservice/CloudServiceApplication.java
@@ -1,9 +1,11 @@
-package com.cloudservice.action;
+package com.cloudservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class CloudServiceApplication {
 
     public static void main(String[] args) {
